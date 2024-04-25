cb7dee00a55f416dd58b33a7eb74d35b4397656e
ericakaroline302007 <111869443+ericakaroline302007@users.noreply.github.com>
2024-04-25 10:24:29 -0300

    ok

--- main.js
+++ main.js
@@ -9,26 +9,26 @@ for (let i = 0; i < botoes.length; i++) {
             textos[j].classList.remove("ativo");
         }
 
-        botoes[i].classList.add("ativo");
+        botoes[iclassList.add("ativo");
         textos[i].classList.add("ativo");
-    }
+    }].
 }
 //cria as constantes e a lista "tempo" dos objetivos ou metas
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-02-02T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-15T00:00:00");
+const tempoObjetivooObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+2 = new Date("2024-12-15T00:00:00");
 const tempoObjetivo3 = new Date("2024-11-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-06-30T00:00:00");
+const tempoObjetivmp
+function calculaTeo4 = new Date("2024-06-30T00:00:00");
 //constante da lista dos tempos de objetivo
-const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
-
-function calculaTempo(tempoObjetivo) {
+const tempos = [tempo(tempoObjetivo) {
     let tempoAtual = new Date(); //obtem a data atual
-    let tempoFinal = tempoObjetivo - tempoAtual; //calcula o tempo final restante
-    let segundos = Math.floor(tempoFinal / 1000); //calcula os segundos restantes
-    let minutos = Math.floor(segundos / 60);//calcula os minutos restantes
-    let horas = Math.floor(minutos / 60);//calcula as horas restantes
-    let dias = Math.floor(horas / 24);//calcula os dias restantes
+    let tempoFinal = th.floor(tempoFinal / 1000); //calcula os segundos restantes
+    let minutos = MathtempoObjetivo - tempoAtual; //calcula o tempo final restante
+    let segundos =th.floor(minutos / 60);//calcula as horas restantes
+    let dias = Mat Ma.floor(segundos / 60);//calcula os minutos restantes
+    let horas = Mah.floor(horas / 24);//calcula os dias restantes
    
     segundos %= 60;  //obtem o resto da divisão dos segundos
     minutos %= 60;   //obtem o resto da divisão dos minutos
